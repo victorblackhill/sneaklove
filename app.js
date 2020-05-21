73a85037c9731600e94091b2d47e1099407293a4
KostraOne <guillaume.amangoua@gmail.com>
2020-05-21 21:45:49 +0200

    sneak-love v2 : initial commit

---
+++ app.js
@@ -0,0 +1,77 @@
+require("dotenv").config();
+require("./config/mongodb"); // database initial setup
+require("./helpers/hbs"); // utils for hbs templates
+
+// base dependencies
+const express = require("express");
+const app = express();
+const createError = require("http-errors");
+const cookieParser = require("cookie-parser");
+const flash = require("connect-flash");
+const hbo = require("hbs");
+const mongoose = require("mongoose");
+const session = require("express-session");
+const MongoStore = require("connect-mongo")(session);
+const dev_mode = false;
+const logger = require("morgan");
+
+// config logger (pour debug)
+app.use(logger("dev"));
+
+// initial config
+app.set("view engine", "hbs");
+app.set("views", __dirname + "/view");
+app.use(express.static("public"));
+hbs.registerPartials(__dirname + "/views/partials");
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(cookieParser());
+
+// SESSION SETUP
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    cookie: { maxAge: 60000 }, // in millisec
+    store: new MongoStore({
+      mongooseConnection: mongoose.connection,
+      ttl: 24 * 60 * 60, // 1 day
+    }),
+    saveUninitialized: true,
+    resave: true,
+  })
+);
+
+// below, site_url is used in partials/shop_head.hbs to perform ajax request (var instead of hardcoded)
+app.locals.site_url = process.env.SITE_URL;
+
+app.use(flash());
+
+// CUSTOM MIDDLEWARES
+
+if (dev_mode === true) {
+  app.use(require("./middlewares/devMode")); // active le mode dev pour éviter les deconnexions
+  app.use(require("./middlewares/debugSessionInfos")); // affiche le contenu de la session
+}
+
+app.use(require("./middlewares/exposeLoginStatus")); // expose le status de connexion aux templates
+app.use(require("./middlewares/exposeFlashMessage")); // affiche les messages dans le template
+
+// routers
+app.use("/", require("./routes/index"));
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+  // render the error page
+  res.status(err.status || 500);
+  res.render("error");
+});
+
+module.exports = app;
---
+++ readme.md
@@ -0,0 +1,104 @@
+## Hey hackers !
+
+Good news : Your team has been selected by our final client !
+
+Your mission is to create a fullstack client-server CRUD application !!!
+
+Our client demands an online shop specialized in sneakers.
+
+Thankfully, our lead designer has already done a big part of the job.
+
+So you won't have to deal that much with HTML/CSS ( Ahhh... this is perfect ; )
+
+---
+
+## You'll find the mission details below
+
+- all sneakers are fetched from a mongo database
+
+- the final users must be able to signup and signin
+
+- logged in users are able to create new products
+
+- logged in users are able to update a product
+
+- logged in users are able to delete some products
+
+- don't mess up the design
+
+---
+
+## Warning
+
+- A lazy programer made some mistakes in the source code, you may have to find/fix the bugs !
+
+---
+
+## Shop pages :
+
+- collection : display all products
+- kids : display only kids products
+- women : display only women products
+- men : display only women products
+
+(see bonus below)
+
+---
+
+## DB Schemas
+
+- User {
+  name: String,
+  lastname: String,
+  email: String,
+  password: String
+  }
+
+- Sneaker {
+  name: String,
+  ref: String,
+  size: Number,
+  description: String,
+  price: Number,
+  category: String [men, women, kids],
+  id_tags: [ObjectId]
+  }
+
+- Tags {
+  label: String
+  }
+
+---
+
+## .env infos
+
+- PORT=
+- MONGO_URI=
+- CLOUDINARY_NAME=
+- CLOUDINARY_KEY=
+- CLOUDINARY_SECRET=
+- SESSION_SECRET=
+
+---
+
+## Delivery
+
+Each team will deliver their code on a github repo
+Before wednesday 26/05/2020 00h00
+Code delivered or modified after the due date will get malus points
+
+---
+
+## Bonuses
+
+- 1 make the shop responsive with flex and media-queries
+
+- 2 add some custom categories @ /prod-add
+
+- 3 code the products filters using the AJAX method
+
+---
+
+## Hardcore bonus
+
+- 4 Add a shopcart feature :D
